b10b7588f235646498e3f201dd2f70f6c6350f7a
Valerii Kukharchuk <valerii.kukharchuk@gmail.com>
2017-01-13 12:36:35 +0000

    it was finished the new viewing of the from_ad

--- lesson6/form_ad.php
+++ lesson6/form_ad.php
@@ -41,8 +41,8 @@
             
             <div class="form-row-indented"> 
                 <label class="form-label-checkbox" for="allow_mails"> 
-                    <input type="checkbox" 
-                           value=<?php echo '"',$showing_ad[SHOWING_AD_ALLOW_MAILS],'"';?> name="allow_mails" 
+                    <input type="checkbox" value="1" name="allow_mails"
+                        <?php echo $showing_ad[SHOWING_AD_ALLOW_MAILS];?>
                            id="allow_mails" class="form-input-checkbox">
                     <span class="form-text-checkbox">Я не хочу получать вопросы по объявлению по e-mail</span> 
                 </label> 
@@ -119,86 +119,96 @@
                 <select title="Выберите категорию объявления" name="category_id" id="fld_category_id" class="form-input-select"> 
                     <option value="">-- Выберите категорию --</option>
                     <optgroup label="Транспорт">
-                        <option value="9">Автомобили с пробегом</option>
-                        <option value="109">Новые автомобили</option>
-                        <option value="14">Мотоциклы и мототехника</option>
-                        <option value="81">Грузовики и спецтехника</option>
-                        <option value="11">Водный транспорт</option>
-                        <option value="10">Запчасти и аксессуары</option>
+                        <?php print_options_of_category(CATEGORY_TRANSPORT,$showing_ad[SHOWING_AD_CATEGORY_ID]);?>
+                        <!--<option value="9">Автомобили с пробегом</option>-->
+                        <!--<option value="109">Новые автомобили</option>-->
+                        <!--<option value="14">Мотоциклы и мототехника</option>-->
+                        <!--<option value="81">Грузовики и спецтехника</option>-->
+                        <!--<option value="11">Водный транспорт</option>-->
+                        <!--<option value="10">Запчасти и аксессуары</option>-->
                     </optgroup>
                     
                     <optgroup label="Недвижимость">
-                        <option value="24">Квартиры</option>
-                        <option value="23">Комнаты</option>
-                        <option value="25">Дома, дачи, коттеджи</option>
-                        <option value="26">Земельные участки</option>
-                        <option value="85">Гаражи и машиноместа</option>
-                        <option value="42">Коммерческая недвижимость</option>
-                        <option value="86">Недвижимость за рубежом</option>
+                        <?php print_options_of_category(CATEGORY_TRANSPORT,$showing_ad[SHOWING_AD_CATEGORY_ID]);?>
+                        <!--<option value="24">Квартиры</option>-->
+                        <!--<option value="23">Комнаты</option>-->
+                        <!--<option value="25">Дома, дачи, коттеджи</option>-->
+                        <!--<option value="26">Земельные участки</option>-->
+                        <!--<option value="85">Гаражи и машиноместа</option>-->
+                        <!--<option value="42">Коммерческая недвижимость</option>-->
+                        <!--<option value="86">Недвижимость за рубежом</option>-->
                     </optgroup>
                     
                     <optgroup label="Работа">
-                        <option value="111">Вакансии (поиск сотрудников)</option>
-                        <option value="112">Резюме (поиск работы)</option>
+                        <?php print_options_of_category(CATEGORY_JOB,$showing_ad[SHOWING_AD_CATEGORY_ID]);?>
+                        <!--<option value="111">Вакансии (поиск сотрудников)</option>-->
+                        <!--<option value="112">Резюме (поиск работы)</option>-->
                     </optgroup>
                     
                     <optgroup label="Услуги">
-                        <option value="114">Предложения услуг</option>
-                        <option value="115">Запросы на услуги</option>
+                        <?php print_options_of_category(CATEGORY_SERVICIES,$showing_ad[SHOWING_AD_CATEGORY_ID]);?>
+                        <!--<option value="114">Предложения услуг</option>-->
+                        <!--<option value="115">Запросы на услуги</option>-->
                     </optgroup>
                     
                     <optgroup label="Личные вещи">
-                        <option value="27">Одежда, обувь, аксессуары</option>
-                        <option value="29">Детская одежда и обувь</option>
-                        <option value="30">Товары для детей и игрушки</option>
-                        <option value="28">Часы и украшения</option>
-                        <option value="88">Красота и здоровье</option>
+                        <?php print_options_of_category(CATEGORY_PERSONALIA,$showing_ad[SHOWING_AD_CATEGORY_ID]);?>
+                        <!--<option value="27">Одежда, обувь, аксессуары</option>-->
+                        <!--<option value="29">Детская одежда и обувь</option>-->
+                        <!--<option value="30">Товары для детей и игрушки</option>-->
+                        <!--<option value="28">Часы и украшения</option>-->
+                        <!--<option value="88">Красота и здоровье</option>-->
                     </optgroup>
                     
                     <optgroup label="Для дома и дачи">
-                        <option value="21">Бытовая техника</option>
-                        <option value="20">Мебель и интерьер</option>
-                        <option value="87">Посуда и товары для кухни</option>
-                        <option value="82">Продукты питания</option>
-                        <option value="19">Ремонт и строительство</option>
-                        <option value="106">Растения</option>
+                        <?php print_options_of_category(CATEGORY_FOR_HOME,$showing_ad[SHOWING_AD_CATEGORY_ID]);?>
+                        <!--<option value="21">Бытовая техника</option>-->
+                        <!--<option value="20">Мебель и интерьер</option>-->
+                        <!--<option value="87">Посуда и товары для кухни</option>-->
+                        <!--<option value="82">Продукты питания</option>-->
+                        <!--<option value="19">Ремонт и строительство</option>-->
+                        <!--<option value="106">Растения</option>-->
                     </optgroup>
                     
                     <optgroup label="Бытовая электроника">
-                        <option value="32">Аудио и видео</option>
-                        <option value="97">Игры, приставки и программы</option>
-                        <option value="31">Настольные компьютеры</option>
-                        <option value="98">Ноутбуки</option>
-                        <option value="99">Оргтехника и расходники</option>
-                        <option value="96">Планшеты и электронные книги</option>
-                        <option value="84">Телефоны</option>
-                        <option value="101">Товары для компьютера</option>
-                        <option value="105">Фототехника</option>
+                        <?php print_options_of_category(CATEGORY_CONSUMER_ELECTRONICS,$showing_ad[SHOWING_AD_CATEGORY_ID]);?>
+                        <!--<option value="32">Аудио и видео</option>-->
+                        <!--<option value="97">Игры, приставки и программы</option>-->
+                        <!--<option value="31">Настольные компьютеры</option>-->
+                        <!--<option value="98">Ноутбуки</option>-->
+                        <!--<option value="99">Оргтехника и расходники</option>-->
+                        <!--<option value="96">Планшеты и электронные книги</option>-->
+                        <!--<option value="84">Телефоны</option>-->
+                        <!--<option value="101">Товары для компьютера</option>-->
+                        <!--<option value="105">Фототехника</option>-->
                     </optgroup>
                     
                     <optgroup label="Хобби и отдых">
-                        <option value="33">Билеты и путешествия</option>
-                        <option value="34">Велосипеды</option>
-                        <option value="83">Книги и журналы</option>
-                        <option value="36">Коллекционирование</option>
-                        <option value="38">Музыкальные инструменты</option>
-                        <option value="102">Охота и рыбалка</option>
-                        <option value="39">Спорт и отдых</option>
-                        <option value="103">Знакомства</option>
+                        <?php print_options_of_category(CATEGORY_HOBBY,$showing_ad[SHOWING_AD_CATEGORY_ID]);?>
+                        <!--<option value="33">Билеты и путешествия</option>-->
+                        <!--<option value="34">Велосипеды</option>-->
+                        <!--<option value="83">Книги и журналы</option>-->
+                        <!--<option value="36">Коллекционирование</option>-->
+                        <!--<option value="38">Музыкальные инструменты</option>-->
+                        <!--<option value="102">Охота и рыбалка</option>-->
+                        <!--<option value="39">Спорт и отдых</option>-->
+                        <!--<option value="103">Знакомства</option>-->
                     </optgroup>
                     
                     <optgroup label="Животные">
-                        <option value="89">Собаки</option>
-                        <option value="90">Кошки</option>
-                        <option value="91">Птицы</option>
-                        <option value="92">Аквариум</option>
-                        <option value="93">Другие животные</option>
-                        <option value="94">Товары для животных</option>
+                        <?php print_options_of_category(CATEGORY_ANIMALS,$showing_ad[SHOWING_AD_CATEGORY_ID]);?>
+                        <!--<option value="89">Собаки</option>-->
+                        <!--<option value="90">Кошки</option>-->
+                        <!--<option value="91">Птицы</option>-->
+                        <!--<option value="92">Аквариум</option>-->
+                        <!--<option value="93">Другие животные</option>-->
+                        <!--<option value="94">Товары для животных</option>-->
                     </optgroup>
                     
                     <optgroup label="Для бизнеса">                        
-                        <option value="116">Готовый бизнес</option>
-                        <option value="40">Оборудование для бизнеса</option>
+                        <?php print_options_of_category(CATEGORY_BUSINESS,$showing_ad[SHOWING_AD_CATEGORY_ID]);?>
+                        <!--<option value="116">Готовый бизнес</option>-->
+                        <!--<option value="40">Оборудование для бизнеса</option>-->
                     </optgroup>
                 </select> 
             </div>
@@ -239,17 +249,21 @@
             
             <div id="f_title" class="form-row f_title"> 
                 <label for="fld_title" class="form-label">Название объявления</label> 
-                <input type="text" maxlength="50" class="form-input-text-long" value="" name="title" id="fld_title"> 
+                <input type="text" maxlength="50" class="form-input-text-long" 
+                value=<?php echo '"',$showing_ad[SHOWING_AD_TITLE],'"';?> name="title" id="fld_title"> 
             </div>
             
             <div class="form-row"> 
                 <label for="fld_description" class="form-label" id="js-description-label">Описание объявления</label> 
-                <textarea maxlength="3000" name="description" id="fld_description" class="form-input-textarea"></textarea> 
+                <textarea maxlength="3000" name="description" id="fld_description" class="form-input-textarea">
+                    <?php echo $showing_ad[SHOWING_AD_DESCRIPTION];?>
+                </textarea> 
             </div>
             
             <div id="price_rw" class="form-row rl"> 
                 <label id="price_lbl" for="fld_price" class="form-label">Цена</label> 
-                <input type="text" maxlength="9" class="form-input-text-short" value="0" name="price" id="fld_price">
+                <input type="text" maxlength="9" class="form-input-text-short" 
+                value=<?php echo '"',$showing_ad[SHOWING_AD_PRICE],'"';?> name="price" id="fld_price">
                 &nbsp;<span id="fld_price_title">руб.</span> 
                 <a class="link_plain grey right_price c-2 icon-link" id="js-price-link" href="/info/pravilnye_ceny?plain">
                     <span>Правильно указывайте цену</span>
--- lesson6/lesson6-index.php
+++ lesson6/lesson6-index.php
@@ -8,35 +8,139 @@ const ACTION_ADD = 'add';
 const ACTION_REMOVE = 'remove';
 const ACTION_FILL_FORM = 'fill_form';
 
-const CITIES = array(
-    "641780" => 'Новосибирск',
-    "641490" => 'Барабинск',
-    "641510" => 'Бердск',
-    "641600" => 'Искитим',
-    "641630" => 'Колывань',
-    "641680" => 'Краснообск',
-    "641710" => 'Куйбышев',
-    "641760" => 'Мошково',
-    "641790" => 'Обь',
-    "641800" => 'Ордынское',
-    "641970" => 'Черепаново'
-);
+// const CITIES = array(
+if( !defined('CITIES') ) {
+    define('CITIES', serialize ( array (
+// $CITIES = array (
+        "641780" => 'Новосибирск',
+        "641490" => 'Барабинск',
+        "641510" => 'Бердск',
+        "641600" => 'Искитим',
+        "641630" => 'Колывань',
+        "641680" => 'Краснообск',
+        "641710" => 'Куйбышев',
+        "641760" => 'Мошково',
+        "641790" => 'Обь',
+        "641800" => 'Ордынское',
+        "641970" => 'Черепаново'
+    // );
+    
+    )) );
+}
 
-const METRO_STATIONS = array(
-    "2028" => 'Берёзовая роща',
-    "2018" => 'Гагаринская',
-    "2017" => 'Заельцовская',
-    "2029" => 'Золотая Нива',
-    "2019" => 'Красный проспект',
-    "2027" => 'Маршала Покрышкина',
-    "2021" => 'Октябрьская',
-    "2025" => 'Площадь Гарина-Михайловского',
-    "2020" => 'Площадь Ленина',
-    "2024" => 'Площадь Маркса',
-    "2022" => 'Речной вокзал',
-    "2026" => 'Сибирская',
-    "2023" => 'Студенческая'
-);
+// const METRO_STATIONS = array(
+if( !defined('METRO_STATIONS') ) {
+    define('METRO_STATIONS', serialize( array (
+// $METRO_STATIONS = array (
+        "2028" => 'Берёзовая роща',
+        "2018" => 'Гагаринская',
+        "2017" => 'Заельцовская',
+        "2029" => 'Золотая Нива',
+        "2019" => 'Красный проспект',
+        "2027" => 'Маршала Покрышкина',
+        "2021" => 'Октябрьская',
+        "2025" => 'Площадь Гарина-Михайловского',
+        "2020" => 'Площадь Ленина',
+        "2024" => 'Площадь Маркса',
+        "2022" => 'Речной вокзал',
+        "2026" => 'Сибирская',
+        "2023" => 'Студенческая'
+        // );
+    )));
+}
+
+//categor'es names
+const CATEGORY_TRANSPORT = 'transport';
+const CATEGORY_REAL_ESTATE = 'real_estate';
+const CATEGORY_JOB = 'job';
+const CATEGORY_SERVICIES = 'servicies';
+const CATEGORY_PERSONALIA = 'personalia';
+const CATEGORY_FOR_HOME = 'for_home';
+const CATEGORY_CONSUMER_ELECTRONICS = 'consumer_electronics';
+const CATEGORY_HOBBY = 'hobby';
+const CATEGORY_ANIMALS = 'animals';
+
+//arrays of category's options
+if( !defined('CATEGORIES') ) {
+    define('CATEGORIES', serialize( array(    
+// $CATEGORIES = array(    
+        CATEGORY_TRANSPORT => array(
+            "9"   => 'Автомобили с пробегом',
+            "109" => 'Новые автомобили',
+            "14"  => 'Мотоциклы и мототехника',
+            "81"  => 'Грузовики и спецтехника',
+            "11"  => 'Водный транспорт',
+            "10"  => 'Запчасти и аксессуары',
+        ),
+        CATEGORY_REAL_ESTATE => array(
+            "24" => 'Квартиры',
+            "23" => 'Комнаты',
+            "25" => 'Дома, дачи, коттеджи',
+            "26" => 'Земельные участки',
+            "85" => 'Гаражи и машиноместа',
+            "42" => 'Коммерческая недвижимость',
+            "86" => 'Недвижимость за рубежом'
+        ),
+        CATEGORY_JOB => array(
+            "111" => 'Вакансии (поиск сотрудников)',
+            "112" => 'Резюме (поиск работы)'
+        ),
+        CATEGORY_SERVICIES => array(
+            "114" => 'Предложения услуг',
+            "115" => 'Запросы на услуги'
+        ),
+        CATEGORY_PERSONALIA => array(
+            "27" => 'Одежда, обувь, аксессуары',
+            "29" => 'Детская одежда и обувь',
+            "30" => 'Товары для детей и игрушки',
+            "28" => 'Часы и украшения',
+            "88" => 'Красота и здоровье'
+        ),
+        CATEGORY_FOR_HOME => array(
+            "21" => 'Бытовая техника',
+            "20" => 'Мебель и интерьер',
+            "87" => 'Посуда и товары для кухни',
+            "82" => 'Продукты питания',
+            "19" => 'Ремонт и строительство',
+            "106" => 'Растения'
+        ),
+        CATEGORY_CONSUMER_ELECTRONICS => array(
+            "32"  => 'Аудио и видео',
+            "97"  => 'Игры, приставки и программы',
+            "31"  => 'Настольные компьютеры',
+            "98"  => 'Ноутбуки',
+            "99"  => 'Оргтехника и расходники',
+            "96"  => 'Планшеты и электронные книги',
+            "84"  => 'Телефоны',
+            "101" => 'Товары для компьютера',
+            "105" => 'Фототехника'
+        ),
+        CATEGORY_HOBBY => array(
+            "33"  => 'Билеты и путешествия',
+            "34"  => 'Велосипеды',
+            "83"  => 'Книги и журналы',
+            "36"  => 'Коллекционирование',
+            "38"  => 'Музыкальные инструменты',
+            "102" => 'Охота и рыбалка',
+            "39"  => 'Спорт и отдых',
+            "103" => 'Знакомства'
+        ),
+        CATEGORY_ANIMALS => array(
+            "89" => 'Собаки',
+            "90" => 'Кошки',
+            "91" => 'Птицы',
+            "92" => 'Аквариум',
+            "93" => 'Другие животные',
+            "94" => 'Товары для животных'
+        ),
+        CATEGORY_BUSINESS => array(
+            "116" => 'Готовый бизнес',
+            "40"  => 'Оборудование для бизнеса'
+        )
+        // );
+    )));
+}
+
 
 //keys of showing_ad
 const SHOWING_AD_RADIO_PRIVATE_PERSON = 'radio_private_person';
@@ -50,21 +154,12 @@ const SHOWING_AD_LOCATION_ID = 'location_id';
 const SHOWING_AD_METRO_ID = 'metro_id';
 const SHOWING_AD_TITLE = 'title';
 const SHOWING_AD_PRICE = 'price';
+const SHOWING_AD_CATEGORY_ID = 'category_id';
+const SHOWING_AD_DESCRIPTION = 'description';
 
 
 const FORM_PARAMS_PRIVATE = 'private';
 
-const ARR_FORM_PARAMS = array(
-    SHOWING_AD_SELLER_NAME,
-    SHOWING_AD_MANAGER,
-    SHOWING_AD_EMAIL,
-    SHOWING_AD_ALLOW_MAILS,
-    SHOWING_AD_PHONE,
-    SHOWING_AD_LOCATION_ID,
-    SHOWING_AD_METRO_ID,
-    SHOWING_AD_TITLE,
-    SHOWING_AD_PRICE
-);
 
 //array contains values of all placeholders for the 'form_add'
 $showing_ad = array(
@@ -78,31 +173,32 @@ $showing_ad = array(
     SHOWING_AD_LOCATION_ID => '641780',
     SHOWING_AD_METRO_ID => '',
     SHOWING_AD_TITLE => '',
-    SHOWING_AD_PRICE => ''
+    SHOWING_AD_PRICE => '0',
+    SHOWING_AD_CATEGORY_ID => '',
+    SHOWING_AD_DESCRIPTION => ''
 );
 
-function print_options_of_cities($id) {
-    foreach (CITIES as $key => $value) {
-        echo '<option ', $id == $key ? 'selected=""' : '', 
-                'data-coords=",," value="',$key,'">',$value,'</option>\n';
+
+function print_any_options($options, $selected_id, $attr) {
+    foreach ($options as $key => $value) {
+        echo '<option ', $selected_id == $key ? 'selected="" ' : ' ', 
+                $attr,'" value="',$key,'">',$value,'</option>\n';
     }
+}
+
+function print_options_of_cities($id) {
+    $arr = unserialize(CITIES); 
+    print_any_options($arr, $id, 'data-coords=",,"');
 }
 
 function print_options_of_metro_stations($id) {    
-    foreach (METRO_STATIONS as $key => $value) {
-        echo '<option ', $id == $key ? 'selected=""' : '', 
-                ' value="',$key,'">',$value,'</option>\n';
-    }
+    $arr = unserialize(METRO_STATIONS);
+    print_any_options($arr, $id, '');
 }
 
-function select_radio_private_person(&$ad) {
-    $ad[SHOWING_AD_RADIO_COMPANY] = '';
-    $ad[SHOWING_AD_RADIO_PRIVATE_PERSON] = 'checked=""';
-}
-
-function select_radio_company(&$ad) {
-    $ad[SHOWING_AD_RADIO_COMPANY] = 'checked=""';
-    $ad[SHOWING_AD_RADIO_PRIVATE_PERSON] = '';
+function print_options_of_category($category, $id) {
+    $arr = unserialize(CATEGORIES);
+    print_any_options($arr[$category], $id, '');
 }
 
 
@@ -124,17 +220,27 @@ switch( filter_input(INPUT_SERVER,'REQUEST_METHOD') ) {
                 $ad = $_SESSION[$key_session];
                 
                 foreach ($ad as $key => $value) {
-//                    if(array_key_exists($key, ARR_FORM_PARAMS) ) {
-//                        $showing_ad[ ARR_FORM_PARAMS[$key] ] = $value;
-//                    }
                     if(array_key_exists($key, $showing_ad) ) {
                         $showing_ad[ $key ] = $value;
                     }
                 }
                 
+                
+                if( $ad[SHOWING_AD_ALLOW_MAILS] == 1 ) {
+                    $showing_ad[SHOWING_AD_ALLOW_MAILS] = 'checked';
+                } else {
+                    $showing_ad[SHOWING_AD_ALLOW_MAILS] = '';
+                }
+                
                 switch( $ad[FORM_PARAMS_PRIVATE] ) {
-                    case '1' : select_radio_company($showing_ad); break;
-                    case '0' : select_radio_private_person($showing_ad); break;
+                    case '1' : 
+                        $showing_ad[SHOWING_AD_RADIO_COMPANY] = 'checked=""';
+                        $showing_ad[SHOWING_AD_RADIO_PRIVATE_PERSON] = '';
+                        break;
+                    case '0' : 
+                        $showing_ad[SHOWING_AD_RADIO_COMPANY] = '';
+                        $showing_ad[SHOWING_AD_RADIO_PRIVATE_PERSON] = 'checked=""';
+                        break;
                 }
             }    
         }
